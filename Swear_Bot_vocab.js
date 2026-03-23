6876e7b3ce9871510f44d9ad731be6b086f1a716
Hound4004 <hound04004@gmail.com>
2026-03-23 18:59:26 -0500

    Create Swear_Bot_vocab.js

---
+++ Swear_Bot_vocab.js
@@ -0,0 +1,3 @@
+var bad_words = ["fuck", "fk", "damn", "shit","bitch","bitc", "fu ", "hound4004 sucks", "dick", "ass", "you suck", "the owner sucks", "this server sucks", "the n word" /*i dont want to type this*/]
+
+module.exports = {bad_words};
