b95277fce850cc295b5504f930fc9246fcaec26d
Hound4004 <hound04004@gmail.com>
2026-03-23 18:12:05 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const server = express()
+
+server.all("/", (req, res) => {
+  res.send("Bot is walking!")})
+function keepAlive() {server.listen(3000, () => {
+    console.log("Server is ready.")})}
+module.exports = keepAlive
