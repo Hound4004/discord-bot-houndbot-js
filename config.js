77dbdea1ebaf5eac9a52c9a0c45d8217eec7a4a0
Hound4004 <hound04004@gmail.com>
2026-03-23 18:08:56 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Discord_Video_channel: process.env.DISCORD_Video_ID, 
+   messageTemplateMCN: "**<@&1107297102788300911>** **<@&1107294677171974207>**  **{author}** just Posted a News Video: **{title}**! **{url}** go check it out!👍",
+    messageTemplate: "**<@&1107294677171974207>**, **{author}** just uploaded a video **{title}**! **{url}** go check it out!👍",
+    messageTemplateSHT: "**<@&1112062613640200292>**, **{author}** just uploaded a short **{title}**! **{url}** go check it out!👍",
+    channel_id: process.env.CHANNEL_Video_ID,
+    watchInterval: 80000
+}
