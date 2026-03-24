8d1b34f783452b5d171742b9c2061eae1cf71ffb
Hound4004 <hound04004@gmail.com>
2026-03-24 13:17:00 -0500

    Update Swear_Bot_vocab.js
    added a description to the page

--- Swear_Bot_vocab.js
+++ Swear_Bot_vocab.js
@@ -1,3 +1,10 @@
+// ============================================================================================================
+// This is Hound4004's Discord bot called Houndbot4004 for his Discorder Server "Houndcord4004" 
+// This bot's Swear_bot_vocab.js was last updated (3/24/2026)
+// This document contains a list of bad words, this is the restricted words of Hound4004's discord! 
+// it is a seperate file intentionaly!
+// ============================================================================================================
+
 var bad_words = [
   "hound4004 sucks",
   "the owner sucks", 
