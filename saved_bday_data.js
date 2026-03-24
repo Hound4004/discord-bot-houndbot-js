3ccb3fd36b6ab7e8b33eb2c4d1ad4a8a12581f22
Hound4004 <hound04004@gmail.com>
2026-03-24 18:31:45 -0500

    Delete saved_bday_data.js
    deleate saved_bday_data file

--- saved_bday_data.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  Hound4004: "si|538743025698865154|ei sb|15012007|eb",
-  xamotex1000: "si|450702721763508235|ei sb|16052008|eb",
-  DoubleZPlaying: "si|589992186490126338|ei sb|30072006|eb",
- }
