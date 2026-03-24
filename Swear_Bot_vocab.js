2b9b9e2ddaf677f756a680e1ada2f61767007feb
Hound4004 <hound04004@gmail.com>
2026-03-24 12:15:59 -0500

    Update Swear_Bot_vocab.js
    updated sward bot vocab list

--- Swear_Bot_vocab.js
+++ Swear_Bot_vocab.js
@@ -1,3 +1,22 @@
-var bad_words = ["fuck", "fk", "damn", "shit","bitch","bitc", "fu ", "hound4004 sucks", "dick", "ass", "you suck", "the owner sucks", "this server sucks", "the n word" /*i dont want to type this*/]
+var bad_words = [
+  "hound4004 sucks",
+  "the owner sucks", 
+  "this server sucks",
+  
+  "fuck", 
+  "fk",
+  "fck",
+  "fu",
+  "damn", 
+  "dam",
+  "shit",
+  "bitch",
+  "bitc",
+  "dick", 
+  "ass", 
+  "you suck", 
+  "the n word", /*i dont want to type this*/
+  "nig"
+  ]
 
 module.exports = {bad_words};
