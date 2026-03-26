df819b6cef59d89cc9c2fc7ea589e806cfc298d2
Hound4004 <hound04004@gmail.com>
2026-03-25 19:10:44 -0500

    Update config.js
    re commented out the current config code to be the un-pinged test code again!

--- config.js
+++ config.js
@@ -4,20 +4,20 @@
 // This is for mesage templates, and other changeable things used in the main index
 // ============================================================================================================
 
-module.exports = {
+/*module.exports = {
     Discord_Video_channel: process.env.DISCORD_Video_ID, 
     messageTemplateMCN: "**<@&1107297102788300911>** **<@&1107294677171974207>**  **{author}** just Posted a News Video: **{title}**! **{url}** go check it out!👍",
     messageTemplate: "**<@&1107294677171974207>**, **{author}** just uploaded a video **{title}**! **{url}** go check it out!👍",
     messageTemplateSHT: "**<@&1112062613640200292>**, **{author}** just uploaded a short **{title}**! **{url}** go check it out!👍",
     channel_id: process.env.CHANNEL_Video_ID,
     watchInterval: 80000
-}
+}*/
 
 // ============================================================================================================
 //the following code to be un-commented when the bot is being worked on to prevent un-nesisary pinging
 // ============================================================================================================
 
-/*var Wip_Msg = " I am being tested right now, this is only here because my brains are being torn apart! This is a much better message than getting pinged!"
+var Wip_Msg = " I am being tested right now, this is only here because my brains are being torn apart! This is a much better message than getting pinged!"
 module.exports = {
     Discord_Video_channel: process.env.DISCORD_Video_ID, 
     messageTemplateMCN: Wip_Msg,
@@ -25,4 +25,4 @@ module.exports = {
     messageTemplateSHT: Wip_Msg,
     channel_id: process.env.CHANNEL_Video_ID,
     watchInterval: 80000
-}*/
+}
